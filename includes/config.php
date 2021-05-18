1994dd21a830953a08f6664f301ac0f6a0de2757
djahern-max <danielaherniv@gmail.com>
2021-05-17 22:35:31 -0400

    ititial commit

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@ session_start();
 date_default_timezone_set("America/New_York");
 
 try {
-    $con = new PDO("mysql:dbname=flipstock;host=localhost:3307", "root", "");
+    $con = new PDO("mysql:dbname=heroku_c3ecb6a7a15fde6;host=us-cdbr-east-03.cleardb.com", "b74c44936ce731", "2b30f1df");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
