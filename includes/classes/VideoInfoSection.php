2ad7737fc38e5cd6736c1601dd45d902654c1efb
djahern-max <danielaherniv@gmail.com>
2021-12-04 11:35:50 -0500

    formatting

--- includes/classes/VideoInfoSection.php
+++ includes/classes/VideoInfoSection.php
@@ -78,7 +78,7 @@ class VideoInfoSection {
                     HOW SHOUD YOU POSITION:
                     <br/>
                     <br/>
-                    It is very rare to hear a money manager recommend holding cash.  Ray Dalio, who started the worlds largest hedge fund Bridgewater Associates, often preaches the dangers of holding cash.  However, this week it was refreshing to hear Dan Niles, the founder of the Santori Fund, state on CNBC that he believes it is important to hold cash right now.  That is a rare statement coming from a fund manager.  I believe that we are at a point in the cycle that “Cash is King” again.  Sure, inflation might be running at ~6% annually right now, which is not good.  But, if you were invested in the NASDAQ on Friday, you lost around 2% of your money….   in ONE DAY!   Many of us have experienced some great gains in 2021 with the DJIA up 12.98%, the NASDAQ up 17.05% and the S&P up 20.83%, so what is the danger of taking money off the table while waiting for better opportunities.  For this reason, I am currently 100% in cash. 
+                    It is very rare to hear a money manager recommend holding cash.  Ray Dalio, who started the worlds largest hedge fund Bridgewater Associates, often preaches the dangers of holding cash.  However, this week it was refreshing to hear Dan Niles, the founder of the Santori Fund, state on CNBC that he believes it is important to hold cash right now.  That is a rare statement coming from a fund manager.  I believe that we are at a point in the cycle that “Cash is King” again.  Sure, inflation might be running at ~6% annually right now, which is not good.  But, if you were invested in the NASDAQ on Friday, you lost around 2% of your money….   in ONE DAY!   Many of us have experienced some great gains in 2021 with the DJIA up 12.98%, the NASDAQ up 17.05% and the S&P up 20.83%, so what is the danger of taking money off the table while waiting for better opportunities.  For this reason, I am currently in 100% in cash. 
                     <br/>
                     <br/>
                     WHERE ARE THE OPPORTUNITIES: 
@@ -93,7 +93,7 @@ class VideoInfoSection {
                     The video above is simply a took I use to review watchlists I have on my phone.  It is helpful for me to review trends over time.  With this strategy, I have been able to earn great investment returns and consistently pick winners.  I hope you find it useful (and not too boring ; ).
                     <br/>
                     <br/>
-                    (The app I use is called “StocksTracker.”  It is a free app that can be downloaded from the app store.  It is priceless to me.  No pricey Boomberg Terminals here!  With this app, and lots of research, I have been able to achieve a 1,571% return over the past three years).
+                    (The app I use is called “StocksTracker.”  It is a free app that can be downloaded from the app store.  It is priceless to me.  No pricey Boomberg Terminals here!).
                     <br/>
                     <br/>
                     Good luck!!
