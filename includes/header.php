f634f0edb5dce356e2386b169e17fdf306b49958
djahern-max <danielaherniv@gmail.com>
2021-12-05 11:17:58 -0500

    update favicon

--- includes/header.php
+++ includes/header.php
@@ -17,8 +17,8 @@ $userLoggedInObj = new User($con, $usernameLoggedIn);
 <html>
 <head>
     <title>Flipstock</title>
-    <link rel="shortcut icon" type="image/png" href="assets/images/icons/favicon.png">
-         <img src="assets/images/icons/favicon.png" title="logo" alt="Site logo">
+    <link rel="shortcut icon" type="image/jpg" href="assets/images/icons/FS.jpg">
+         <img src="assets/images/icons/FS.jpg" title="logo" alt="Site logo">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
