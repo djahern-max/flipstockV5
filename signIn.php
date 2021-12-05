a18277f4b7a3de09a28594ac67e5b196d078e83c
djahern-max <danielaherniv@gmail.com>
2021-12-05 11:26:18 -0500

    updating logo

--- signIn.php
+++ signIn.php
@@ -46,7 +46,7 @@ function getInputValue($name) {
         <div class="column">
 
             <div class="header">
-                <!-- <img src="assets/images/icons/favicon.png" title="logo" alt="Site logo"> -->
+                <img src="assets/images/icons/favicon.jpg" title="logo" alt="Site logo">
                 <h3>Sign In</h3>
                 <span>to continue to Flipstock</span>
             </div>
